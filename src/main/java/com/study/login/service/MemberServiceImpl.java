a624c276a79170c84e273b70bd4aa6ff685a7ed7
Jaeyoon <wodbs4338@naver.com>
2021-04-18 03:51:50 +0900

    패스워드 암호화 적용하여 멤버 save 로직 구현

--- src/main/java/com/study/login/service/MemberServiceImpl.java
+++ src/main/java/com/study/login/service/MemberServiceImpl.java
@@ -1,14 +1,45 @@
 package com.study.login.service;
 
 import com.study.login.domain.dto.MemberDto;
+import com.study.login.repository.Member;
+import com.study.login.repository.MemberRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class MemberServiceImpl implements MemberService{
 
+    private final MemberRepository memberRepository;
+    private final Encryption encryption;
+
+    public MemberServiceImpl(MemberRepository memberRepository, Encryption encryption) {
+        this.memberRepository = memberRepository;
+        this.encryption = encryption;
+    }
+
     @Override
     public String save(MemberDto member) {
-        return member.getId();
+
+        try{
+            String pw = member.getPassword();
+            String salt = encryption.getSalt();
+            String encryptedPassword = encryption.getEncryptedPassword(pw, salt);
+            ModelMapper modelMapper = new ModelMapper();
+            Member memberEntity = modelMapper.map(member, Member.class);
+
+            memberEntity.setPassword(encryptedPassword);
+            memberEntity.setSalt(salt);
+
+            memberRepository.save(memberEntity);
+
+            return memberEntity.getId();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
