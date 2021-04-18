56461e22ec88089d146f5c88d9ad80c64459a216
Jaeyoon <wodbs4338@naver.com>
2021-04-19 03:07:54 +0900

    회원가입시 세션 세팅

--- src/main/java/com/study/login/web/LoginController.java
+++ src/main/java/com/study/login/web/LoginController.java
@@ -3,9 +3,13 @@ package com.study.login.web;
 import com.study.login.domain.dto.MemberDto;
 import com.study.login.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -17,14 +21,30 @@ public class LoginController {
         this.memberService = memberService;
     }
 
+    @GetMapping("signup")
+    public String signup() {
+        return "signup";
+    }
+
     @PostMapping("signup")
-    public String signUp(@RequestBody Map<String,Object> params) {
+    public String signUp(@RequestBody Map<String,Object> params, HttpSession session) {
         String id = (String) params.get("userId");
         String pw = (String) params.get("userPw");
 
+        // System.out.println("params : "+params);
+
         memberService.save(new MemberDto(id,pw));
 
-        return id;
+        session.setAttribute("loginId", id);
+        // session.setMaxInactiveInterval(30); // 세션 지속 시간
+
+        return null;
     }
 
+    @GetMapping("main")
+    public String hello(Model model, HttpServletRequest request) {
+        String loginId = (String) request.getSession().getAttribute("loginId");
+        model.addAttribute("data", loginId);
+        return "main";
+    }
 }
