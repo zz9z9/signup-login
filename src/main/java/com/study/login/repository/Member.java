aef8c8c6686f8092fe56f900bef344fd672d656e
Jaeyoon <wodbs4338@naver.com>
2021-04-20 00:18:46 +0900

    @ToString 추가

--- src/main/java/com/study/login/repository/Member.java
+++ src/main/java/com/study/login/repository/Member.java
@@ -2,12 +2,15 @@ package com.study.login.repository;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "MEMBER")
+@ToString
 public class Member {
 
     @Id
