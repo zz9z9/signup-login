de2b8b976ad6cb516ee9abb5fe00c28c0fcba4b6
Jaeyoon <wodbs4338@naver.com>
2021-04-21 02:15:04 +0900

    security 설정 파일 수정 및 filter에서 request에 유효한 사용자 id 세팅

--- src/main/java/com/study/login/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/study/login/security/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.study.login.security.jwt;
 
+import com.study.login.repository.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,15 +20,24 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("JwtAuthenticationFilter.doFilter");
         // 헤더에서 JWT 를 받아옵니다.
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
         // 유효한 토큰인지 확인합니다.
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            System.out.println("authentication : "+authentication);
             // SecurityContext 에 Authentication 객체를 저장합니다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
+
+//            System.out.println("authentication.getName() : "+authentication.getName());
+//            System.out.println("authentication.getPrincipal() : "+authentication.getPrincipal());
+
+            request.setAttribute("userId", authentication.getName());
         }
+
+
         chain.doFilter(request, response);
     }
 }
