0d29556098cc9d3b59b82951ba22855d4717d7d7
Jaeyoon <wodbs4338@naver.com>
2021-04-21 02:29:01 +0900

    userDetailService 관련 파일 추가 및 UserDetails implement

--- src/main/java/com/study/login/repository/Member.java
+++ src/main/java/com/study/login/repository/Member.java
@@ -1,17 +1,25 @@
 package com.study.login.repository;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "MEMBER")
 @ToString
-public class Member {
+public class Member implements UserDetails {
 
     @Id
     private String id;
@@ -19,5 +27,39 @@ public class Member {
     private String password;
     @Column
     private String salt;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
---
+++ src/main/java/com/study/login/service/MemberDetailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.study.login.service;
+
+import com.study.login.repository.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberDetailServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberDetailServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String pk) throws UsernameNotFoundException {
+        return memberRepository.findById(pk);
+    }
+}
