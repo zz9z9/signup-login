64f828e294c265cb3eafdf24b9fa20900b4dc4fe
Jaeyoon <wodbs4338@naver.com>
2021-04-22 03:28:44 +0900

    주석 정리

--- src/main/java/com/study/login/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/study/login/security/jwt/JwtAuthenticationFilter.java
@@ -20,19 +20,12 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("JwtAuthenticationFilter.doFilter");
-        // 헤더에서 JWT 를 받아옵니다.
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request); // 헤더에서 JWT 를 받아옵니다.
+
         // 유효한 토큰인지 확인합니다.
         if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            System.out.println("authentication : "+authentication);
-            // SecurityContext 에 Authentication 객체를 저장합니다.
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-//            System.out.println("authentication.getName() : "+authentication.getName());
-//            System.out.println("authentication.getPrincipal() : "+authentication.getPrincipal());
+            Authentication authentication = jwtTokenProvider.getAuthentication(token); // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
+            SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContext 에 Authentication 객체를 저장합니다.
 
             request.setAttribute("loginId", authentication.getName());
         }
--- src/main/java/com/study/login/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/study/login/security/jwt/JwtTokenProvider.java
@@ -43,8 +43,7 @@ public class JwtTokenProvider {
                 .setClaims(claims) // 정보 저장
                 .setIssuedAt(now) // 토큰 발행 시간 정보
                 .setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
-                .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과
-                // signature 에 들어갈 secret값 세팅
+                .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과 signature 에 들어갈 secret값 세팅
                 .compact();
     }
 
