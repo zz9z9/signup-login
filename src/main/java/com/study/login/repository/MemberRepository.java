d82e98124db6900da65d6c70d45930fd36feffa2
Jaeyoon <wodbs4338@naver.com>
2021-04-29 01:04:08 +0900

    MemberRepository findById 선언할 필요 없으므로 삭제

--- src/main/java/com/study/login/repository/MemberRepository.java
+++ src/main/java/com/study/login/repository/MemberRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member,Long> {
-    Member findById(String id);
+public interface MemberRepository extends JpaRepository<Member,String> {
 }
